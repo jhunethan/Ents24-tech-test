dc55f59850981a8bd4fb7caef0ed9b9ea9bb1867
jhunethan <jhunethan23@gmail.com>
2021-08-11 23:26:36 +0100

    added npm run check script

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-module.exports.generateVenueURI = function (town, venue) {
+const axios = require("axios");
+
+function generateVenueURI(town, venue) {
   if (!town || !venue) return "";
 
   function normaliseString(string) {
@@ -9,4 +11,18 @@ module.exports.generateVenueURI = function (town, venue) {
   const formattedVenue = normaliseString(venue);
 
   return `/${formattedTown}-events/${formattedVenue}`;
-};
+}
+
+function checkVenue() {
+  const [,town,,venue] = process.env.npm_lifecycle_script.split('"');
+  const URI = generateVenueURI(town, venue);
+
+  axios
+    .get(`https://www.ents24.com${URI}`)
+    .then((data) => {
+      console.log(data.status)
+    })
+    .catch((err) => console.log(err.response.status));
+}
+
+module.exports = { generateVenueURI, checkVenue };
