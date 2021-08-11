fbc9c7bc6b6d0e97a616eb1ae87e96fcac28d44d
jhunethan <jhunethan23@gmail.com>
2021-08-11 22:31:55 +0100

    bad code committed

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
 module.exports.generateVenueURI = function (town, venue) {
-  if (!town || !venue) return "";
-
-  function normaliseString(string) {
-    return string.trim().toLowerCase().replaceAll(" ", "-");
-  }
-
-  const formattedTown = normaliseString(town);
-  const formattedVenue = normaliseString(venue);
-
-  return `/${formattedTown}-events/${formattedVenue}`;
+  // bad code here
+  return `bad code here`;
 };
