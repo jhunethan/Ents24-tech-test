0ab297481bb4a34f3115e1bebea7b3dcde3bd6a4
jhunethan <jhunethan23@gmail.com>
2021-08-11 23:44:05 +0100

    abstracted normaliseString

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ function generateVenueURI(town, venue) {
   if (!town || !venue) return "";
 
   function normaliseString(string) {
-    return string.trim().toLowerCase().replaceAll(" ", "-");
+    if (!string) return "";
+    let formattedString = "";
+    formattedString = string.trim();
+    formattedString = formattedString.toLowerCase();
+    formattedString = formattedString.replaceAll(" ", "-");
+
+    return formattedString;
   }
 
   const formattedTown = normaliseString(town);
@@ -14,13 +20,13 @@ function generateVenueURI(town, venue) {
 }
 
 function checkVenue() {
-  const [,town,,venue] = process.env.npm_lifecycle_script.split('"');
+  const [, town, , venue] = process.env.npm_lifecycle_script.split('"');
   const URI = generateVenueURI(town, venue);
 
   axios
     .get(`https://www.ents24.com${URI}`)
     .then((data) => {
-      console.log(data.status)
+      console.log(data.status);
     })
     .catch((err) => console.log(err.response.status));
 }
